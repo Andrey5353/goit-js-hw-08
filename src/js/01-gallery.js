36f0d57c0b1b5d6fa89115ed4f0bd1897e124526
Andrey5353 <andreyae0117ab@gmail.com>
2022-09-06 15:39:27 +0300

    js-hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+list.innerHTML = elemnt(galleryItems);
+
+function elemnt(gallery) {
+    const imageEl = gallery.map(
+        ({ preview, original, description }) =>
+            `<a class="gallery__item" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+ </a>`
+    ).join('');
+
+    return imageEl;
+};
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionsDelay: 250 });
+
